0741afbc85ca061c592f6a4dcb2d2f8a07ef58e8
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-09-18 14:23:25 -0400

    cleaned up comments in server.js and added first draft of README.md

--- server.js
+++ server.js
@@ -8,15 +8,10 @@ const db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static directory
 app.use(express.static("public"));
 
-// Routes
-// =============================================================
 require("./routes/api-routes.js")(app);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
