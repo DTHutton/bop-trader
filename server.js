21e285936690b2b3369710931887127f8edef5cc
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-09-18 14:18:43 -0400

    setup server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
