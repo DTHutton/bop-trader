cb269ee3b8a6c195483c4b90ab1cb51e4d4a81d1
Austin Alber <austinalber@gmail.com>
2019-09-26 19:45:07 -0400

    Fixed eslint errors

--- config/passport.js
+++ config/passport.js
@@ -2,73 +2,76 @@
 // const sequelize = require('sequelize');
 // not sure if we need to require passport or sequelize here ^^
 
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
+var LocalStrategy = require("passport-local").Strategy;
+var bcrypt = require("bcryptjs");
 
 // Load User Model
-const User = require('../models/user');
+var User = require("../models/user");
 
 module.exports = function(passport) {
+  console.log("hit passport.js file");
 
-	console.log("hit passport.js file");
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email"
+      },
+      (email, password, done) => {
+        // When a user tries to sign in this code runs
 
-	passport.use(
-		new LocalStrategy(
-			{
-				usernameField: 'email'
-			},
-			(email, password, done) => {
-				// When a user tries to sign in this code runs
+        // Match user (check if user already has an email registered)
+        User.findOne({
+          email: email
+        })
+          .then(user => {
+            // if no email match, return done
+            if (!user) {
+              return done(null, false, {
+                message: "That email is not regisetered"
+              });
+            }
 
-				// Match user (check if user already has an email registered)
-				User.findOne({
-					email: email
-				})
-					.then((user) => {
-						// if no email match, return done
-						if (!user) {
-							return done(null, false, {
-								message: 'That email is not regisetered'
-							});
-						}
+            // If there is a user with given email, check the password
 
-						// If there is a user with given email, check the password
+            // Match password user submits, check againt Database stored hash to see if it matches
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+              if (err) {
+                throw err;
+              }
 
-						// Match password user submits, check againt Database stored hash to see if it matches
-						bcrypt.compare(password, user.password, (err, isMatch) => {
-							if (err) throw err;
+              if (isMatch) {
+                console.log(
+                  "\n account verified - set the user with done function..\n"
+                );
+                console.log("time to work on bcrypt..");
+                return done(null, user);
+              } else {
+                return done(null, false, {
+                  message: "Password incorrect"
+                });
+              }
+              // if none of the above should i return the user? >>  or is that the catch.
+              // return done(null, user);
+            });
+          })
+          .catch(err => console.log(err));
+      }
+    )
+  );
 
-							if (isMatch) {
-								console.log('\n account verified - set the user with done function..\n');
-								console.log("time to work on bcrypt..")
-								return done(null, user);
-							} else {
-								return done(null, false, {
-									message: 'Password incorrect'
-								});
-							}
-							// if none of the above should i return the user? >>  or is that the catch.
-							// return done(null, user);
-						});
-					})
-					.catch((err) => console.log(err));
-			}
-		)
-	);
+  // In order to help keep authentication state across HTTP requests,
+  // Sequelize needs to serialize and deserialize the user
 
-	// In order to help keep authentication state across HTTP requests,
-	// Sequelize needs to serialize and deserialize the user
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+    console.log(". . >> passport.serializeUser HIT");
+    // example only returned user. not user.id
+  });
 
-	passport.serializeUser((user, done) => {
-		done(null, user.id);
-		console.log('. . >> passport.serializeUser HIT');
-		// example only returned user. not user.id
-	});
-
-	passport.deserializeUser((id, done) => {
-		User.findById(id, (err, user) => {
-			console.log('. . >> passport.serializeUser HIT');
-			done(err, user);
-		});
-	});
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      console.log(". . >> passport.serializeUser HIT");
+      done(err, user);
+    });
+  });
 };
--- public/js/register.js
+++ public/js/register.js
@@ -1,37 +1,44 @@
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function (event) {
-    event.preventDefault();
-    console.log("handleFormSubmit HIT on @ register.js public file");
-  
-    let user = {
-      username: $("name").val().trim(),
-      email: $("email").val().trim(),
-      password: $("password").val().trim(),
-      password2: $("password2").val().trim()
-    };
-  
-    // const example = {
-    //   text: $exampleText.val().trim(),
-    //   description: $exampleDescription.val().trim()
-    // };
-  
-    if (!(user.username && user.email)) {
-      alert("You must enter an example text and description!");
-      return;
-    }
-  
-    API.saveExample(example).then(function () {
-      refreshExamples();
-    });
-  
-    // $exampleText.val("");
-    // $exampleDescription.val("");
+var handleFormSubmit = function(event) {
+  event.preventDefault();
+  console.log("handleFormSubmit HIT on @ register.js public file");
 
-    console.log("REGISTER.JS HIT > > >   user.username = ", user.username)
-
-    res.render("form submitted.")
-
+  const user = {
+    username: $("name")
+      .val()
+      .trim(),
+    email: $("email")
+      .val()
+      .trim(),
+    password: $("password")
+      .val()
+      .trim(),
+    password2: $("password2")
+      .val()
+      .trim()
   };
 
-  $("#submit").on("click", handleFormSubmit)
+  // const example = {
+  //   text: $exampleText.val().trim(),
+  //   description: $exampleDescription.val().trim()
+  // };
+
+  if (!(user.username && user.email)) {
+    alert("You must enter an example text and description!");
+    return;
+  }
+
+  API.saveExample(example).then(function() {
+    refreshExamples();
+  });
+
+  // $exampleText.val("");
+  // $exampleDescription.val("");
+
+  console.log("REGISTER.JS HIT > > >   user.username = ", user.username);
+
+  res.render("form submitted.");
+};
+
+$("#submit").on("click", handleFormSubmit);
