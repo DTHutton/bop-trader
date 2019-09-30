0a923c4ebc952e8830d2f30531ef08048514d22a
Thomas White <thomas.white.ucf@gmail.com>
2019-09-30 17:36:22 -0400

    push to sync

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-//   // Getting references to our form and inputs
-//   // var loginForm = $("form.login");
+  // Getting references to our form and inputs
+  // var loginForm = $("form.login");
 
-//   // When the form is submitted, we validate there's an email and password entered
-$("form.login").on("submit", function(event) {
+  // When the form is submitted, we validate there's an email and password entered
+  $("form.login").on("submit", function(event) {
     var emailInput = $("#email-input");
     var passwordInput = $("#password-input");
     event.preventDefault();
@@ -11,23 +11,25 @@ $("form.login").on("submit", function(event) {
       email: emailInput.val(),
       password: passwordInput.val()
     };
-    $.post("/login", userData).then(res => {
-      console.log('the res', res);
-      window.location.replace('/index');
-    }).catch(err => console.log('the errrrrrr', err));
+    $.post("/login", userData)
+      .then(res => {
+        console.log("the res", res);
+        window.location.replace("/index");
+      })
+      .catch(err => console.log("the errrrrrr", err));
   });
 
-//     console.log("userData = ", userData);
+  //     console.log("userData = ", userData);
 
-//     if (!userData.email || !userData.password) {
-//       return;
-//     }
+  //     if (!userData.email || !userData.password) {
+  //       return;
+  //     }
 
-//     // If we have an email and password we run the loginUser function and clear the form
-//     loginUser(userData.email, userData.password);
-//     emailInput.val("");
-//     passwordInput.val("");
-//   });
+  // If we have an email and password we run the loginUser function and clear the form
+  //     loginUser(userData.email, userData.password);
+  //     emailInput.val("");
+  //     passwordInput.val("");
+  //   });
 
   // loginUser does a post to our "/login" route and if successful, redirects us the the dashboard page
   // function loginUser(email, password) {
