3d189a883e3a4cfe6f28a26711159323cc192f82
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-09-29 21:48:57 -0400

    added btcTicker.js file. moved code for BTC value in header to the file and removed it from main.handlebars.

---
+++ public/js/btcTicker.js
@@ -0,0 +1,41 @@
+ //Fetch the value of Bitcoin
+ const api_url = 'https://api.cryptonator.com/api/ticker/btc-usd';
+ // Log initial price at page loadup
+ console.log(bitcoinDataHandler().toString());
+ const time_interval = 2;
+ function addLeadingZero(num) {
+   return (num <= 9) ? ("0" + num) : num;
+ }
+ function clientDateTime() {
+   var date_time = new Date();
+   var curr_hour = date_time.getHours();
+   var zero_added_curr_hour = addLeadingZero(curr_hour);
+   var curr_min = date_time.getMinutes();
+   var curr_sec = date_time.getSeconds();
+   var curr_time = zero_added_curr_hour + ':' + curr_min + ':' + curr_sec;
+   return curr_time
+ }
+ function makeHttpObject() {
+   try { return new XMLHttpRequest(); }
+   catch (error) { }
+ }
+ function bitcoinGetData() {
+   var request = makeHttpObject();
+   request.open("GET", api_url, false);
+   request.send(null);
+   return request.responseText;
+ }
+ function bitcoinDataHandler() {
+   var raw_data_string = bitcoinGetData();
+   var data = JSON.parse(raw_data_string);
+   var base = data.ticker.base;
+   var target = data.ticker.target;
+   var price = data.ticker.price;
+   var volume = data.ticker.volume;
+   var change = data.ticker.change;
+   var api_server_epoch_timestamp = data.timestamp;
+   var api_success = data.success;
+   var api_error = data.error;
+   return price;
+ }
+ document.getElementById("btc_val").innerHTML = "BTC - " + "$" + Math.round(bitcoinDataHandler()) + " USD";
